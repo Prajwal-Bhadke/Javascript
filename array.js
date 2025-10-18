f5f1dc7af73beb3d885d5e0a7d76096a2786f5db
Prajwal-Bhadke <pbhadke3@gmail.com>
2025-10-18 18:12:15 +0530

    these small block of code has been created while practicing or we can say watching apna college javascript lectures

---
+++ array.js
@@ -0,0 +1,62 @@
+//// FOR LOOP ON ARRAY
+
+// let arr = [12, 11, 33, 24, 25, 23];
+// for (let ind=0; ind < arr.length; ind++ ){
+//     console.log(arr[ind]);
+// }
+
+// FOR OF ON ARRAY
+
+// let str = ['prajwal','bhadke'];
+// for ( let num of str){
+//     console.log(num.toUpperCase())
+// }
+
+// PRACTICE QUE 
+
+// let sum =0;
+// for (let val of arr){
+//     sum += val;
+// }
+// let avg = sum / arr.length;
+// console.log(`avg of the given values is = ${avg}`)
+
+// let aaa = [250, 645, 300, 900, 50];
+// for (let i = 0; i < aaa.length; i++) {
+//   let offer = aaa[i] / 10;
+//   aaa[i] -= offer;
+// }
+// console.log(aaa);
+
+//unshift 
+
+// let str = ["prajwal", "bhadke"];
+// console.log(str);
+// str.unshift();
+// console.log(str);
+
+// splice
+
+// let aaa = [250, 645, 300, 900, 50];
+// aaa.splice(2,2)
+// console.log
+
+// ARROW FUNTION
+
+// const sum = (a, b) => {
+//   console.log(a + b);
+// };
+// sum(3, 4);
+
+
+// forEach on array 
+
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// arr.forEach((val, ind) => {
+//   console.log(val**2);
+// });
+
+
+// .map() on ARRAY
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
